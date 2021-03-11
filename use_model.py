a849b68c6fdaa74cc654fbbe40d2825c8897c52e
Srikanth Roopa <srikanth.roopa1@gmail.com>
2021-03-11 08:23:05 +0100

    Load the model and scalar and test

---
+++ use_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 08:20:45 2021
+
+@author: srikanth
+"""
+
+import pickle
+
+import numpy as np
+
+local_classifier = pickle.load(open('classifier.pickle','rb'))
+local_scaler = pickle.load(open('sc.pickle','rb'))
+
+new_pred = local_classifier.predict(local_scaler.transform(np.array([[40,20000]])))
+new_pred_proba = local_classifier.predict_proba(local_scaler.transform(np.array([[40,20000]])))[:,1]
+
+new_pred_2 = local_classifier.predict(local_scaler.transform(np.array([[42,50000]])))
+new_pred_proba_2 = local_classifier.predict_proba(local_scaler.transform(np.array([[42,50000]])))[:,1]
+
+
